dd47ce80f0696cae9fc19a22ed3a009af400d88b
bastengao <bastengao@gmail.com>
2018-03-26 17:39:51 +0800

    Format code

--- lib/aliyun/cloudpush/config.rb
+++ lib/aliyun/cloudpush/config.rb
@@ -1,31 +1,31 @@
 module Aliyun
-    module Cloudpush
-      class << self
-        attr_accessor :config
-  
-        def configure
-          yield self.config ||= Config.new
-        end
+  module Cloudpush
+    class << self
+      attr_accessor :config
 
-        def default_client
-            @client ||= Client.new(app_key, access_key_id, access_key_secret)
-        end
-  
-        def app_key
-          @app_key ||= config.app_key
-        end
-  
-        def access_key_id
-          @access_key_id ||= config.access_key_id
-        end
-  
-        def access_key_secret
-          @access_key_secret ||= config.access_key_secret
-        end
+      def configure
+        yield self.config ||= Config.new
       end
-  
-      class Config
-        attr_accessor :app_key, :access_key_id, :access_key_secret
+
+      def default_client
+        @client ||= Client.new(app_key, access_key_id, access_key_secret)
+      end
+
+      def app_key
+        @app_key ||= config.app_key
+      end
+
+      def access_key_id
+        @access_key_id ||= config.access_key_id
+      end
+
+      def access_key_secret
+        @access_key_secret ||= config.access_key_secret
       end
     end
+
+    class Config
+      attr_accessor :app_key, :access_key_id, :access_key_secret
+    end
   end
+end
